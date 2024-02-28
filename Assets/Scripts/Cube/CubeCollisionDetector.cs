bb8330bb9f6fdfa5d2a8a786ff839680d6d73060
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-02-28 15:46:08 +0200

    collision detector

---
+++ Assets/Scripts/Cube/CubeCollisionDetector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cube
+{
+    public class CubeCollisionDetector : MonoBehaviour
+    {
+        public event Action<PointsContainer> onCollisionDetect;
+
+        
+        
+        private void OnCollisionEnter(Collision col)
+        {
+            var colContainer = col.gameObject.GetComponent<PointsContainer>();
+            if (colContainer == null)
+                return;
+            
+            onCollisionDetect?.Invoke(colContainer);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Cube/CubeCollisionMerge.cs
@@ -0,0 +1,59 @@
+﻿using UI;
+using UnityEngine;
+using Zenject;
+
+namespace Cube
+{
+    public class CubeCollisionMerge : MonoBehaviour
+    {
+        private const int STATIC_DEGREE = 2;
+
+        [SerializeField] private int _touchForce = 5;
+
+        private PointsContainer _pointsContainer;
+        private Rigidbody _rigidbody;
+        private CubeCollisionDetector _detector;
+
+        private UiPointsCalculate _uiPointsCalculate;
+        
+        [Inject]
+        private void Construct(UiPointsCalculate uiPointsCalculate)
+        {
+            _uiPointsCalculate = uiPointsCalculate;
+        }
+        
+        private void Awake()
+        {
+            _pointsContainer = GetComponent<PointsContainer>();
+            _rigidbody = GetComponent<Rigidbody>();
+            _detector = GetComponent<CubeCollisionDetector>();
+        }
+
+
+        private void MergeCube(PointsContainer col)
+        {
+            if (_pointsContainer.Points == col.Points)
+            {
+                OnTouchCube();
+                _pointsContainer.Points *= STATIC_DEGREE;
+                Destroy(col.gameObject);
+                _uiPointsCalculate.CalculatePoints(col.Points);
+            }
+        }
+
+        private void OnTouchCube()
+        {
+            _rigidbody.AddForce(transform.up * _touchForce, ForceMode.Impulse);
+        }
+        
+        private void OnEnable()
+        {
+            _detector.onCollisionDetect += MergeCube;
+        }
+
+        private void OnDisable()
+        {
+            _detector.onCollisionDetect -= MergeCube;
+        }
+    }
+}
