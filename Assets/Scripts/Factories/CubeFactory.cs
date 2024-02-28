5dbccb17166c717c0c5b69d39e64eda1286fe877
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-02-28 15:45:29 +0200

    Factory cube

---
+++ Assets/Scripts/Factories/CubeFactory.cs
@@ -0,0 +1,23 @@
+﻿using Components;
+using UnityEngine;
+
+namespace Factories
+{
+    public class CubeFactory
+    {
+        private readonly CubeComponent.Factory _factory;
+
+        public CubeFactory(CubeComponent.Factory factory)
+        {
+            _factory = factory;
+        }
+
+        public CubeComponent SpawnCube(Vector3 spawnPos)
+        {
+            var cube = _factory.Create();
+            cube.transform.position = spawnPos;
+            return cube;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Factories/CubeSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using Components;
+using UnityEngine;
+using Zenject;
+
+namespace Factories
+{
+    public class CubeSpawner : MonoBehaviour
+    {
+        public bool readyToSpawn;
+        public CubeComponent CubeComponent { get; private set; }
+        
+        [SerializeField] private Vector3 startPos;
+        [SerializeField] private float delay = 0.3f;
+        
+        private CubeFactory _cubeFactory;
+        private Coroutine _coroutine;
+        
+        [Inject]
+        private void Construct(CubeFactory cubeFactory)
+        {
+            _cubeFactory = cubeFactory;
+        }
+
+        private void Start()
+        {
+            Spawn();
+            _coroutine = StartCoroutine("SpawnCubeWithDelay");
+        }
+
+        public void Spawn()
+        {
+            CubeComponent = _cubeFactory.SpawnCube(startPos);
+        }
+
+        IEnumerator SpawnCubeWithDelay()
+        {
+            while (true)
+            {
+                while (readyToSpawn == false)
+                {
+                    yield return null;
+                }
+                
+                yield return new WaitForSeconds(delay);
+                Spawn();
+                readyToSpawn = false;
+            }
+        }
+    }
+}
